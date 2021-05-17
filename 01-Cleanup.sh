3f81c4933d7f5637bf8527c71a52c0f697ea0da5
Neo2003 <Neo2003@users.noreply.github.com>
2021-05-17 19:57:25 +0200

    Change name and remove commands for later

--- 01-Cleanup.sh
+++ 01-Cleanup.sh
@@ -5,9 +5,3 @@ systemctl disable hciuart.service
 systemctl disable raspi-config.service
 apt-get install busybox-syslogd
 apt-get remove --purge rsyslog
-rm -rf /var/lib/dhcp /var/lib/dhcpcd5 /var/spool /etc/resolv.conf
-ln -s /tmp /var/lib/dhcp
-ln -s /tmp /var/lib/dhcpcd5
-ln -s /tmp /var/spool
-touch /tmp/dhcpcd.resolv.conf
-ln -s /tmp/dhcpcd.resolv.conf /etc/resolv.conf 
