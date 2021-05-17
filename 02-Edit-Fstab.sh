8704045e987467ff95324bd0dfaee429b04b8010
Neo2003 <Neo2003@users.noreply.github.com>
2021-05-17 20:08:11 +0200

    Add the next file

---
+++ 02-Edit-Fstab.sh
@@ -0,0 +1,4 @@
+echo Follow intructions to update /etc/fstab displayed on GitHub
+read -p "Press enter to open the editor, make changes then quit with Ctrl+X then Y (O if French)"
+nano /etc/fstab
+
