304e9ae97bac025345461a6afa36b77f65932161
Neo2003 <Neo2003@users.noreply.github.com>
2021-05-17 19:58:25 +0200

    Make these commands run later

---
+++ 03-Complete-RO.sh
@@ -0,0 +1,6 @@
+rm -rf /var/lib/dhcp /var/lib/dhcpcd5 /var/spool /etc/resolv.conf
+ln -s /tmp /var/lib/dhcp
+ln -s /tmp /var/lib/dhcpcd5
+ln -s /tmp /var/spool
+touch /tmp/dhcpcd.resolv.conf
+ln -s /tmp/dhcpcd.resolv.conf /etc/resolv.conf 
