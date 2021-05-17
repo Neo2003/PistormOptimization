d81a9d3e6653928fe8d2601d5223aa25d9ff773b
Neo2003 <Neo2003@users.noreply.github.com>
2021-05-17 21:56:05 +0200

    Prepare files

--- 01-Cleanup.sh
+++ 01-Cleanup.sh
@@ -1,3 +1,6 @@
+chmod +x 02-Edit-Fstab.sh
+chmod +x 03-Complete-RO.sh
+chmod +x 04-Move-emulator.sh
 apt-get remove --purge triggerhappy logrotate dphys-swapfile avahi*
 apt-get autoremove --purge
 systemctl disable apt-daily.service
