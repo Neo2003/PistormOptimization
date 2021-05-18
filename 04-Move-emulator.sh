8b7958ff62db3b4da2cceac94949cac204cede36
Neo2003 <Neo2003@users.noreply.github.com>
2021-05-18 17:39:39 +0200

    Update 04-Move-emulator.sh

--- 04-Move-emulator.sh
+++ 04-Move-emulator.sh
@@ -3,3 +3,5 @@ mount /mnt/Amiga
 chmod ugo+rwx /mnt/Amiga/
 mv /home/pi/pistorm /mnt/Amiga/
 chown -R pi:pi /mnt/Amiga/pistorm
+cp Pistorm.service /etc/systemd/system/
+systemctl enable Pistorm.service
