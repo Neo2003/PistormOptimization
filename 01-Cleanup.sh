614d63eda4f18f694e9ba588f596124173003bde
Neo2003 <Neo2003@users.noreply.github.com>
2021-05-18 18:06:44 +0200

    Silent APT

--- 01-Cleanup.sh
+++ 01-Cleanup.sh
@@ -1,10 +1,10 @@
 chmod +x 02-Edit-Fstab.sh
 chmod +x 03-Complete-RO.sh
 chmod +x 04-Move-emulator.sh
-apt-get remove --purge triggerhappy logrotate dphys-swapfile avahi*
-apt-get autoremove --purge
+apt-get -y remove --purge triggerhappy logrotate dphys-swapfile avahi*
+apt-get -y autoremove --purge
 systemctl disable apt-daily.service
 systemctl disable hciuart.service
 systemctl disable raspi-config.service
-apt-get install busybox-syslogd
-apt-get remove --purge rsyslog
+apt-get -y install busybox-syslogd
+apt-get -y remove --purge rsyslog
