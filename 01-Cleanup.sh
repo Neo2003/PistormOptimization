02d1118d037aac0a247f30650f869f9be2375eb1
Neo2003 <Neo2003@users.noreply.github.com>
2021-05-18 19:13:50 +0200

    Another service disabled

--- 01-Cleanup.sh
+++ 01-Cleanup.sh
@@ -6,5 +6,6 @@ apt-get -y autoremove --purge
 systemctl disable apt-daily.service
 systemctl disable hciuart.service
 systemctl disable raspi-config.service
+systemctl disable rpi-eeprom-update.service
 apt-get -y install busybox-syslogd
 apt-get -y remove --purge rsyslog
