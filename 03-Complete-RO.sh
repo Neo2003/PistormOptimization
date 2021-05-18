274a6c9d70c420a2c64bb93b264de3dc695fdfcb
Neo2003 <Neo2003@users.noreply.github.com>
2021-05-18 22:32:05 +0200

    Automatize a bit more

--- 03-Complete-RO.sh
+++ 03-Complete-RO.sh
@@ -3,4 +3,6 @@ ln -s /tmp /var/lib/dhcp
 ln -s /tmp /var/lib/dhcpcd5
 ln -s /tmp /var/spool
 touch /tmp/dhcpcd.resolv.conf
-ln -s /tmp/dhcpcd.resolv.conf /etc/resolv.conf 
+ln -s /tmp/dhcpcd.resolv.conf /etc/resolv.conf
+cat bash_logout > /etc/bash.bash_logout
+cat bashrc >> /etc/bash.bashrc
